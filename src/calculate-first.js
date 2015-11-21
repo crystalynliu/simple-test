de3b493e45a340cc524cc5998af25177a1c58adf
crystalyn Liu <crystalynliu@163.com>
2015-11-21 18:12:51 +0800

    first fail test

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,16 @@
 function single_element(arr) {
-  
+	var temp =[];
+	var result=[];
+	var isRepeated;
+	for(var i=1;i<=arr.length;i++)
+	{
+		if(i%2===0){
+			temp.push(arr[i-1]);
+		}
+	}
+	for(var i=0;i<temp.length;i++){
+		if (temp.indexOf(temp[i]) < 0) 
+			result.push(temp[i]);
+    }  
+	return result;
 }
