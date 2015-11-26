f35972eda0c17d642aa087b82615940e6bce3835
crystalyn Liu <crystalynliu@163.com>
2015-11-26 13:43:36 +0800

    second:sucessfully complete

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,14 @@
 function calculate_median(arr) {
-
+    var result;
+    var newArr = arr.filter(function(item,i){
+        return i%2===1;
+    }).sort();
+    if(newArr.length%2===1){
+        var position = parseInt(newArr.length/2);
+        result = newArr[position];
+    }else if(newArr.length%2===0){
+        var position = parseInt(newArr.length/2);
+        result=(newArr[position]+newArr[position-1])/2;
+    }
+    return result;
 }
