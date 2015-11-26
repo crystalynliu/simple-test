dfd8e28f69a7c825fb6555ec64b2ae0be5187470
crystalyn Liu <crystalynliu@163.com>
2015-11-26 13:17:01 +0800

    first:change the code by ES5 filter

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,16 +1,8 @@
 function single_element(arr) {
-	var temp =[];
-	var result=[];
-	var isRepeated;
-	for(var i=1;i<=arr.length;i++)
-	{
-		if(i%2===0){
-			temp.push(arr[i-1]);
-		}
-	}
-	for(var i=0;i<temp.length;i++){
-		if (temp.indexOf(temp[i]) === temp.lastIndexOf(temp[i])) 
-			result.push(temp[i]);
-    }  
+    var result = arr.filter(function(item,i){
+		return i%2===1;
+	}).filter(function(item,i,newArr){
+		return newArr.indexOf(item)===newArr.lastIndexOf(item);
+	})
 	return result;
 }
