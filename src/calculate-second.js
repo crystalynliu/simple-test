ea7207d437d196532afea33c73e50be594d4aebd
crystalyn Liu <crystalynliu@163.com>
2015-11-26 13:59:47 +0800

    second:change the code by a function just do one thing and change the sort function

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -2,13 +2,23 @@ function calculate_median(arr) {
     var result;
     var newArr = arr.filter(function(item,i){
         return i%2===1;
-    }).sort();
-    if(newArr.length%2===1){
-        var position = parseInt(newArr.length/2);
-        result = newArr[position];
-    }else if(newArr.length%2===0){
-        var position = parseInt(newArr.length/2);
-        result=(newArr[position]+newArr[position-1])/2;
+    });
+    return median(newArr);
+}
+
+function median(arr){
+    var result;
+    var len = arr.length;
+    var newArr = arr.concat();
+    newArr.sort(sortNum);
+    if(len%2===1){
+        result = newArr[parseInt(len/2)];
+    }else if(len%2===0){
+        result=(newArr[parseInt(len/2)]+newArr[parseInt(len/2)-1])/2;
     }
     return result;
+}
+
+function sortNum(a,b){
+    return a-b;
 }
